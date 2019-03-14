ab27b7672ca2fe34584b5657291bfc4c2e31d0a0
tatiaputri <tatia1999@gmail.com>
2019-03-14 09:26:54 +0700

    string replace : Fungsi utama dari Function str_replace ini adalah untuk menggantikan substring tertentu didalam suatu string.

---
+++ string replace.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php
+echo str_replace("world","Sugik","Hello world!");
+?>
+<p>In this example, we search for the string "Hello World!", find the value "world" and then replace the value with "Sugik".</p>
+</body>
+</html>
